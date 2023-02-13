eedfd10e8b9d301aef3891eb688ffc7b1da47710
John DiNovi <johndinovi@dhcp-10-250-39-69.harvard.edu>
2023-02-13 17:55:21 -0500

    tweak yelp.py

--- yelp.py
+++ yelp.py
@@ -3,7 +3,7 @@ import requests
 
 def lambda_handler(event, context):
     
-    # Business Search URL
+    # Business Search URL, max limit of 50 responses per request
     url = "https://api.yelp.com/v3/businesses/search?sort_by=best_match&limit=50"
 
     # Yelp API Authentication Key
@@ -18,25 +18,29 @@ def lambda_handler(event, context):
     # Generate the content to be displayed in the HTML
     content = "<!DOCTYPE html> <html> <body> <table style='border:1px solid black;margin-left:auto;margin-right:auto;'> <tr> <th style='font-size:150%'>Japanese Restaurants in Boston</th> </tr>"
     
-    # Iterate through the offset values to get more than just 20 restaurants (timeout at 150 restaurants)
-    for off in [0, 50, 100]:
+    # Iterate through offset values to get more than just 20 restaurants
+    for off in range(0, 301, 50):
         
-        # Parameters for search
-        parameters = {
-            'location' : 'Boston',
-            'radius' : 40000,
-            'term' : 'restaurant,japanese,japan',
-            'offset' : off
-        }
-
-        # Make request to API
-        request = requests.get(url=url, headers=headers, params=parameters)
+        try:
+            
+            # Parameters for search
+            parameters = {
+                'location' : 'Boston',
+                'radius' : 40000,
+                'term' : 'restaurant,japanese,japan',
+                'offset' : off
+            }
     
-        # Iterate through and add formatted values to HTML table
-        for rest in request.json()['businesses']:
-            add = '<tr> <td>' + rest['name'] + '</td> </tr>'
-            content += add
-    
+            # Make request to API
+            request = requests.get(url=url, headers=headers, params=parameters)
+        
+            # Iterate through and add formatted values to table in HTML
+            for rest in request.json()['businesses']:
+                add = '<tr> <td>' + rest['name'] + '</td> </tr> '
+                content += add
+        except:
+            break
+        
     content += "</table> </body> </html>"
     
     # Return the HTML content
